74aa72568ea96567dcda496015f7c8927c60c470
bagaschoi <123846796+bagaschoi@users.noreply.github.com>
2023-01-30 18:14:26 +0700

    update link

--- src/commands/bot/info.js
+++ src/commands/bot/info.js
@@ -50,12 +50,12 @@ module.exports = async (client, interaction, args) => {
                 },
                 {
                     name: "🔧┆Bot owner",
-                    value: `<@!755297485328482356> `,
+                    value: `<@!796946355083673620> `,
                     inline: true,
                 },
                 {
                     name: "🔧┆Bot developer",
-                    value: `<@!755297485328482356> <@!884553151666061372>`,
+                    value: `<@!796946355083673620> <@!796946355083673620>`,
                     inline: true,
                 },
                 {
